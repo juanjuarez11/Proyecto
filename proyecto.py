c24d59806d53728deca9d9a45ef807032ad0d5e1
juanjuarez11 <juan.juarez.cmo@gmail.com>
2026-06-03 08:01:48 -0600

    Mejora

--- proyecto.py
+++ proyecto.py
@@ -1,9 +1,14 @@
 import json
+#VER CATALOGO
 try:
     with open("catalogo.json", "r") as archivo:
         catalogo=json.load(archivo)
         for fila in catalogo:
            print(f"Nombre: {fila["Nombre"]} | Precio: {fila["Precio"]} | Cantidad: {fila["Cantidad"]} | Color: {fila["Color"]}")
+except IOError:
+    print("Error: Catalogo no existente")
+#COMPRAR
+try:
     with open("catalogo.json") as archivo:
         catalogo=json.load(archivo)
         compra = input("Que producto desea comprar?: ").capitalize()
@@ -12,5 +17,8 @@ try:
             print(f"Producto: {producto_encontrado["Nombre"]}")
             print(f"Precio: {producto_encontrado["Precio"]}")
             print(f"Cantidad: {producto_encontrado["Cantidad"]}")
+            cantidad = int(input("Cuantas unidades desea comprar?: "))
+            if cantidad <= producto_encontrado["Cantidad"]:
+                producto_encontrado["Precio"] -= cantidad
 except Exception as e:
     print(e)
