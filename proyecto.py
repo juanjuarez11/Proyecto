8ed093fc3b4ad1b12dfe9d28b878092e3e718b7b
JuanJuarez11 <juan.juarez.cmo@gmail.com>
2026-06-05 15:14:17 -0600

    M

--- proyecto.py
+++ proyecto.py
@@ -1,11 +1,25 @@
-import csv
-print(f"{"*" * 5}BIENVENIDO A LA HERMANDAD CHINA{"*" * 5}")
-with open("catalogo.csv") as archivo:
-        lector=csv.reader(archivo)
-        for fila in lector:
-            for producto in fila:
-                print(f" {producto} ",end="")
-            print("")
-print("MENU DE OPCCIONES:")
-print("1. COMPRAR")
-print("2. SALIR")
+import json
+try:
+    with open("credenciales.json", "r") as archivo:
+        credencial = {archivo}
+        print("INICIANDO SESIÓN")
+        autentificacion = input("Por favor, ingrese su usuario: ")
+        if autentificacion in credencial.keys():
+            print("EXCELENTE, USUARIO CORRECTO")
+            contrasena = input("Por favor, Ingrese su contraseña: ")
+            if contrasena in credencial:
+                print("CONTRASEÑA CORRECTA")
+                print("BIENVENIDO AL SISTEMA")
+                print("1. Agregar un producto")
+                print("2. Cargar inventario ya existente")
+                opcion = int(input("Ingrese una opción (1-2): "))
+                match opcion:
+                    case 1:
+                        
+            else:
+                print("CONTRASEÑA INCORRECTA")
+
+        else:
+            print(f"ERROR: {credencial} no está en la lista de usuarios")
+except Exception as e:
+    print("ERROR: {e}")
