906e5ce638ae4603a7cff83cf55a71b1ad611a0e
JuanJuarez11 <juan.juarez.cmo@gmail.com>
2026-06-06 22:13:00 -0600

    Reestructurar bucles y condicionales

--- proyecto.py
+++ proyecto.py
@@ -2,60 +2,78 @@ import json
 try:
     with open("credenciales.json", "r", encoding= "utf-8") as archivo:
         credencial = json.load(archivo)
-        print(credencial)
-        print("BIENVENIDO")
-        inten1=3
-        inten2=3
-        while inten1>0:
+        print("BIENVENIDO AL SISTEMA ACCESO")
+
+        intentos_usuario=3
+        acceso_concedido = False
+
+        while intentos_usuario > 0:
             user = input("Por favor, ingrese su usuario: ")
-            if user in credencial.values():
+
+            if user == credencial["usuario"]:
                 print("USUARIO CORRECTO")
-                while inten2>0:
+                intentos_contrasena = 3
+
+                while intentos_contrasena > 0:
                     contrasena = input("Por favor, Ingrese su contraseña: ")
-                    if contrasena in credencial.values():
-                        print("CONTRASEÑA CORRECTA")
-                        print("INICIANDO SESION")
-                        print("BIENVENIDO AL SISTEMA")
-                        print("1. Agregar productos")
-                        print("2. Cargar inventario ya existente")
-                        opcion = int(input("Ingrese una opción (1-2): "))
-                        match opcion:
-                            case 1:
-                                with open("catalogo.json", "w", encoding="utf-8") as archivo:
-                                    nombre = ""
-                                    precio = 0
-                                    cantidad = 0
-                                    categoria = ""
-                                    cant = int(input("Cuántos productos desea agregar?: "))
-                                    inventario = []
-                                    for i in range(1, cant+1):
-                                        nombre = input(f"Ingrese nombre del producto {i}: ")
-                                        precio = float(input(f"Ingrese el precio del producto {i}: "))
-                                        cantidad = int(input(f"Cantidad de unidades del producto {i}: "))
-                                        categoria = input(f"Ingrese la categoría del producto {i}: ")
-                                        producto = {
-                                            "nombre": nombre,
-                                            "precio": precio,
-                                            "cantidad": cantidad,
-                                            "categoria": categoria}
-                                        inventario.append(producto)
-                                    json.dump(inventario, archivo, indent=4, ensure_ascii=False)
-                            case 2:
-                                try:
-                                    nombre = input("Ingrese el nombre y dominio del archivo: ")
-                                    with open(nombre, "r") as archivo:
-                                        lector = json.load(archivo)
-
-                                except IOError:
-                                    print("ERROR. CATÁLOGO NO ENCONTRADO")
+                    if contrasena == credencial["contrasena"] and not acceso_concedido:
+                        print("CONTRASEÑA CORRECTA. INICIANDO SESIÓN....")
+                        acceso_concedido = True
+                        break
                     else:
-                        inten2-=1
-                        print("CONTRASEÑA INCORRECTA")
-                        print(f"Intentos restantes {inten2}")
-
+                        intentos_contrasena-=1
+                        print(f"CONTRASEÑA INCORRECTA. Intentos restantes: {intentos_contrasena}")
+                if acceso_concedido:
+                        break
             else:
                 inten1-=1
                 print(f"ERROR: {user} no está en la lista de usuarios")
                 print(f"Intentos restantes {inten1}")
+
+    if acceso_concedido:
+        while True:           
+            print("BIENVENIDO AL SISTEMA")
+            print("1. Agregar productos")
+            print("2. Cargar inventario ya existente")
+            print("3. Salir del sistema")
+            opcion = int(input("Ingrese una opción (1-3): "))
+            match opcion:
+                case 1:
+                    with open("catalogo.json", "w", encoding="utf-8") as archivo:
+                        nombre = ""
+                        precio = 0
+                        cantidad = 0
+                        categoria = ""
+                        cant = int(input("Cuántos productos desea agregar?: "))
+                        inventario = []
+                        for i in range(1, cant+1):
+                            nombre = input(f"Ingrese nombre del producto {i}: ")
+                            precio = float(input(f"Ingrese el precio del producto {i}: "))
+                            cantidad = int(input(f"Cantidad de unidades del producto {i}: "))
+                            categoria = input(f"Ingrese la categoría del producto {i}: ")
+                            producto = {
+                                "nombre": nombre,
+                                "precio": precio,
+                                "cantidad": cantidad,
+                                "categoria": categoria}
+                            inventario.append(producto)
+                            json.dump(inventario, archivo, indent=4, ensure_ascii=False)
+                case 2:
+                    try:
+                        nombre = input("Ingrese el nombre y dominio del archivo: ")
+                        with open(nombre, "r") as archivo:
+                            lector = json.load(archivo)
+                    except IOError:
+                        print("ERROR. CATÁLOGO NO ENCONTRADO")
+                case 3:
+                    print("Saliendo del sistema. Se cerró sesión exitosamente")
+                    break
+                case _:
+                    print("Opción no válida. Intente de nuevo.")
+            
+    else:
+        print("Intentos agotados. Saliendo del sistema")
+except FileNotFoundError:
+    print("ERROR CRÍTICO: El archivo 'credenciales.json' no existe.")
 except Exception as e:
     print(f"ERROR: {e}")
